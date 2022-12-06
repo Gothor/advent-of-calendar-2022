46f69c0ec25e790b9eb9fa99556dc647cb24dc50
Rodolphe Peccatte <rodolphe.peccatte@mfa.fr>
2022-12-06 11:16:42 +0100

    December 6th

---
+++ 06/2/script.js
@@ -0,0 +1,30 @@
+/*
+    How to execute:
+    node script.js
+*/
+
+import * as fs from 'node:fs/promises';
+
+const buffer = await fs.readFile('input.txt');
+
+const content = [...buffer.toString()];
+
+const lastChars = [];
+
+let i;
+for (i = 1; i <= content.length; i++) {
+  const c = content[i - 1];
+
+  if (lastChars.length === 14) {
+    lastChars.shift();
+  }
+  lastChars.push(c);
+
+  const nbUniques = lastChars.filter((v, i, arr) => arr.indexOf(v) === i).length;
+
+  if (nbUniques === 14) {
+    break;
+  }
+}
+
+console.log("Position: " + i);
